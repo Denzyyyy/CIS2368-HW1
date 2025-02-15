b44c911a0dae5aa6057fd3f53e52d7c580530f7c
Hugues <izahchidimma@gmail.com>
2025-02-14 22:04:39 -0600

    first commit

---
+++ creds.py
@@ -0,0 +1,5 @@
+class Creds:
+    conString = ''
+    userName = ''
+    password = ''
+    dbName = ''
---
+++ database.py
@@ -0,0 +1,86 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password, dbName):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=dbName
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred", e )
+    return connection
+
+
+def list_values():
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM vaults")
+    vaults = cursor.fetchall()
+    connection.close()
+    
+    print("Vaults:")
+    for vault in vaults:
+        print(vault)
+        
+def create_vault():
+    number = int(input("Enter vault number: "))
+    code = int(input("Enter vault code: "))
+    content = input("Enter vault content: ")
+    owner = input("Enter vault owner: ")
+    
+    
+    connection = create_connection()
+    cursor = connection.cursor()
+    connection.execute("INSERT INTO vaults (number, code, content, owner) VALUES ", (number, code, content, owner))
+    connection.commit()
+    connection.close()
+    
+    print("Vault created")
+    
+def retrieve_vault():
+    number = int(input("Enter vault number: "))
+    code = int(input("Enter vault code: "))
+    
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM vaults WHERE number = %s AND code = %s", (number, code))
+    result = cursor.fetchone()
+    connection.close()
+    
+    if result:
+        print(result)
+        return True
+    else:
+        print("Vault not found")
+        return False
+    
+def main():
+    while True:
+        print("\nBank Management System:")
+        print("1. List vaults")
+        print("2. Create vault")
+        print("3. Retrieve vault")
+        print("4. Exit")
+        
+        choice = int(input("Enter your choice: "))
+        
+        if choice == 1:
+            list_values()
+        elif choice == 2:
+            create_vault()
+        elif choice == 3:
+            retrieve_vault()
+        elif choice == 4:
+            break
+        else:
+            print("Invalid choice")
+            
+if __name__ == "__main__":
+    main()
+
+    
